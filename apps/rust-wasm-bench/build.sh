852f88077caed4dc657444f14e4751426f4a5208
Torre <tostaylo1@gmail.com>
2020-10-04 15:08:38 -0500

    rust-wasm-bench implemented

--- apps/rust-wasm-bench/build.sh
+++ apps/rust-wasm-bench/build.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-wasm-pack build --release --target web --out-name rust-fel-bench
+wasm-pack build --release --target web --out-name rust-wasm-bench
 http
 # or could use python3 -m http.server
