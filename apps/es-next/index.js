7c51429239fd71e1608e487c9c816ceda19970cc
Torre <tostaylo1@gmail.com>
2020-10-19 22:51:47 -0500

    Update functionality for es-next

--- apps/es-next/index.js
+++ apps/es-next/index.js
@@ -16,6 +16,7 @@ const words = [
 ];
 
 let counter = 0;
+let currentRows = 0;
 
 (function run() {
 	const root = document.getElementById('root');
@@ -34,28 +35,37 @@ let counter = 0;
 	const k_button = document.createElement('button');
 	k_button.id = 'create1000';
 	k_button.innerText = 'Create K';
+	k_button.addEventListener('click', () => createTable(1000));
 
 	const ten_k_button = document.createElement('button');
 	ten_k_button.id = 'create10000';
 	ten_k_button.innerText = 'Create 10K';
+	ten_k_button.addEventListener('click', () => createTable(10000));
 
 	const clear_button = document.createElement('button');
 	clear_button.id = 'clear';
 	clear_button.innerText = 'Clear';
+	clear_button.addEventListener('click', clear);
 
-	k_button.addEventListener('click', () => createTable(1000));
-	ten_k_button.addEventListener('click', () => createTable(10000));
-	clear_button.addEventListener('click', () => {
-		document.querySelector('table')?.remove();
-	});
+	const update_button = document.createElement('button');
+	update_button.id = 'update';
+	update_button.innerText = 'Update';
+	update_button.addEventListener('click', updateTableData);
 
 	header.appendChild(h1);
 	header.appendChild(k_button);
 	header.appendChild(ten_k_button);
 	header.appendChild(clear_button);
+	header.appendChild(update_button);
 })();
 
+function clear() {
+	currentRows = 0;
+	document.querySelector('table')?.remove();
+}
+
 function createTable(rows) {
+	currentRows = rows;
 	const oldTable = document.querySelector('table');
 	oldTable?.parentNode?.removeChild(oldTable);
 
@@ -63,13 +73,14 @@ function createTable(rows) {
 		const table = document.createElement('table');
 		const tableBody = document.createElement('tbody');
 
-		for (let i = 0; i < rows; i++) {
+		for (let i = 1; i <= rows; i++) {
 			const idx = i <= 14 ? i + 14 + counter : i + counter;
 			const row = document.createElement('tr');
 			const data1 = document.createElement('td');
 			const data2 = document.createElement('td');
+			data2.id = `td${i}`;
 
-			const data1Text = document.createTextNode((1 + i).toString());
+			const data1Text = document.createTextNode(i.toString());
 			const data2Text = document.createTextNode(`${words[idx % 12]} ${words[idx % 13]} ${words[idx % 14]}`);
 
 			data1.appendChild(data1Text);
@@ -84,4 +95,13 @@ function createTable(rows) {
 		root?.appendChild(table);
 	}
 	counter += counter + 1;
+}
+
+function updateTableData() {
+	let i;
+	for (i = 1; i <= currentRows; i++) {
+		if (i % 10 === 0) {
+			document.getElementById(`td${i}`).innerText = 'We are updated';
+		}
+	}
 }
