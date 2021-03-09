0ef1b07db5344f06f4624f8ccf56b625ef7dec8f
Torre <tostaylo@gmail.com>
2021-03-08 22:17:02 -0600

    adding correct type to file directory names

--- trace-generator/metrics.ts
+++ trace-generator/metrics.ts
@@ -1,15 +1,17 @@
+type FileAndDirectoryNames = 'create-k' | 'create-ten-k' | 'clear-k' | 'clear-ten-k' | 'update-k' | 'update-ten-k';
+
 export type Metric = {
-	fileName: string;
-	dirName: 'create-k' | 'create-ten-k' | 'clear-k' | 'clear-ten-k' | 'update-k' | 'update-ten-k';
+	fileName: FileAndDirectoryNames;
+	dirName: FileAndDirectoryNames;
 	selector: 'button#create1000' | 'button#create10000';
 	selector2?: 'button#clear' | 'button#update';
 };
 
 export const metrics: Metric[] = [
-	{ fileName: 'k', dirName: 'create-k', selector: 'button#create1000' },
-	{ fileName: '10k', dirName: 'create-ten-k', selector: 'button#create10000' },
-	{ fileName: 'clearK', dirName: 'clear-k', selector: 'button#create1000', selector2: 'button#clear' },
-	{ fileName: 'clear10K', dirName: 'clear-ten-k', selector: 'button#create10000', selector2: 'button#clear' },
-	{ fileName: 'updateK', dirName: 'update-k', selector: 'button#create1000', selector2: 'button#update' },
-	{ fileName: 'update10K', dirName: 'update-ten-k', selector: 'button#create10000', selector2: 'button#update' },
+	{ fileName: 'create-k', dirName: 'create-k', selector: 'button#create1000' },
+	{ fileName: 'create-ten-k', dirName: 'create-ten-k', selector: 'button#create10000' },
+	{ fileName: 'clear-k', dirName: 'clear-k', selector: 'button#create1000', selector2: 'button#clear' },
+	{ fileName: 'clear-ten-k', dirName: 'clear-ten-k', selector: 'button#create10000', selector2: 'button#clear' },
+	{ fileName: 'update-k', dirName: 'update-k', selector: 'button#create1000', selector2: 'button#update' },
+	{ fileName: 'update-ten-k', dirName: 'update-ten-k', selector: 'button#create10000', selector2: 'button#update' },
 ];
